578ce8365a9ec72074508877e102799d9c24e26d
sander lemans <sander lemans>
2016-11-23 14:01:49 +0100

    stateEnum

--- workshop/src/clockradio/ClockRadioStateEnum.java
+++ workshop/src/clockradio/ClockRadioStateEnum.java
@@ -8,25 +8,90 @@ import statestack.ContextBase;
  */
 public enum ClockRadioStateEnum implements ClockRadioState {
     NOT_POWERED {
-        // TODO
+        @Override
+        public void powerOn(ContextBase<ClockRadioState, ClockRadioDevice> ctx) {
+            ctx.changeFromToState("Power on", this, POWERED);
+        }
+       
     },
     POWERED {
-        // TODO
+        @Override
+        public void enter(ContextBase ctx) {
+            ctx.addState(IDLE_MODE);
+        }
+
+        @Override
+        public void idleButtonPressed(ContextBase<ClockRadioState, ClockRadioDevice> ctx) {
+            ctx.innerTransition("idleButtonPressed", this, IDLE_MODE);
+        }
+
+        @Override
+        public void alarmButtonPressed(ContextBase<ClockRadioState, ClockRadioDevice> ctx) {
+            ctx.innerTransition("idleButtonPressed", this, ALARM_MODE);
+        }
+
+        @Override
+        public void radioButtonPressed(ContextBase<ClockRadioState, ClockRadioDevice> ctx) {
+            ctx.innerTransition("idleButtonPressed", this, RADIO_MODE);
+        }
+
+        @Override
+        public void powerOff(ContextBase<ClockRadioState, ClockRadioDevice> ctx) {
+            ctx.changeFromToState("Power off", this, NOT_POWERED);
+        }
+        
     },
     IDLE_MODE {
         // TODO
+        
     },
     RADIO_MODE {
+        @Override
+        public void enter(ContextBase ctx) {
+            //TODO device start playing
+            ((ClockRadioDevice)ctx.getDevice()).radio(true);
+        }
         // TODO
+
+        @Override
+        public void exit(ContextBase ctx) {
+            //TODO stop the radio
+            ((ClockRadioDevice)ctx.getDevice()).radio(false);
+        }
+        
     },
     ALARM_MODE {
-        // TODO
+        @Override
+        public void enter(ContextBase ctx) {
+            ctx.addState(ALARM_IDLE);
+        }
+        
     },
     ALARM_IDLE {
-        // TODO
+        @Override
+        public void alarmTimeReached(ContextBase<ClockRadioState, ClockRadioDevice> ctx) {
+            ctx.changeFromToState("Alarm time", this, ALARM_BUZZING);
+        }
     },
     ALARM_BUZZING {
-        // TODO
+        @Override
+        public void enter(ContextBase ctx) {
+            ((ClockRadioDevice)ctx.getDevice()).startBuzzing();
+        }
+
+        @Override
+        public void exit(ContextBase ctx) {
+            ((ClockRadioDevice)ctx.getDevice()).stopBuzzing();
+        }
+
+        @Override
+        public void stopAlarmButtonPressed(ContextBase<ClockRadioState, ClockRadioDevice> ctx) {
+            ctx.changeFromToState("Stop buzzing", this, ALARM_IDLE);
+        }
+        
+        
+        
+        
     },
     NULL {
         @Override
