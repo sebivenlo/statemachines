b31d3753d2d303f22696f83a74c9393d274017c4
Pieter van den Hombergh <p.vandenhonbergh@fontys.nl>
2017-11-15 10:25:58 +0100

    added device and device mock example

--- statewalkertest/src/main/java/nl/fontys/sebivenlo/statewalkertest/Context.java
+++ statewalkertest/src/main/java/nl/fontys/sebivenlo/statewalkertest/Context.java
@@ -8,68 +8,61 @@ import nl.fontys.sebivenlo.statewalker.ContextBase;
  */
 public class Context extends ContextBase<Context, Dev, State> {
 
-    public Context(Class<?> stateClass) {
-        super(stateClass);
-
+    public Context( Class<?> stateClass, Dev d ) {
+        super( stateClass );
+        this.device = d;
     }
 
     void e1() {
-        getTopState().e1(this);
+        getTopState().e1( this );
     }
 
     void e2() {
-        getTopState().e2(this);
+        getTopState().e2( this );
     }
 
     void e3() {
-        getTopState().e3(this);
+        getTopState().e3( this );
     }
 
     void e4() {
-        getTopState().e4(this);
+        getTopState().e4( this );
     }
 
     void e5() {
-        getTopState().e5(this);
+        getTopState().e5( this );
     }
 
     void e6() {
-        getTopState().e6(this);
+        getTopState().e6( this );
     }
 
     void e7() {
-        getTopState().e7(this);
+        getTopState().e7( this );
     }
 
     void e8() {
-        getTopState().e8(this);
+        getTopState().e8( this );
     }
 
     void e9() {
-        getTopState().e9(this);
+        getTopState().e9( this );
     }
 
     void e10() {
-        getTopState().e10(this);
+        getTopState().e10( this );
     }
 
     void e11() {
-        getTopState().e11(this);
+        getTopState().e11( this );
     }
 
     void e12() {
-        getTopState().e12(this);
+        getTopState().e12( this );
     }
 
     void e13() {
-        getTopState().e13(this);
-    }
-
-    Dev dev = new Dev();
-
-    @Override
-    public Dev getDevice() {
-        return dev;
+        getTopState().e13( this );
     }
 
 }
